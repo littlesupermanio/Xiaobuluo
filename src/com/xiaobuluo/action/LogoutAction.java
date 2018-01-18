b4f3e62d2e4d07b350e0a7f42345a725d614e6c5
HoHoibin <imhhb1997@gmail.com>
2018-01-19 02:31:01 +0800

    注销功能

--- src/com/xiaobuluo/action/LogoutAction.java
+++ src/com/xiaobuluo/action/LogoutAction.java
@@ -1,5 +1,7 @@
 package com.xiaobuluo.action;
 
+import com.xiaobuluo.entity.Message;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +11,14 @@ import java.io.IOException;
 
 public class LogoutAction extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        //强制销毁Session
+        request.getSession().removeAttribute("user");
+        Message msg = Message.successMessage("注销成功","/pages/login.jsp");
+        request.setAttribute("message",msg);
+        request.getRequestDispatcher("/pages/message.jsp").forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request,response);
     }
 }
